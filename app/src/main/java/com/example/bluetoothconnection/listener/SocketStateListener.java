2482b4eff9557f42ac491da010efdf657073aaaa
Bahadır Günenç <bahadir.gunenc@bites.local>
2025-01-23 10:12:56 +0300

    db part is deleted

--- app/src/main/java/com/example/bluetoothconnection/listener/SocketStateListener.java
+++ app/src/main/java/com/example/bluetoothconnection/listener/SocketStateListener.java
@@ -1,7 +1,4 @@
 package com.example.bluetoothconnection.listener;
-
-import android.bluetooth.BluetoothSocket;
-
 import com.example.bluetoothconnection.utilities.BluetoothClientThread;
 
 public interface SocketStateListener {
